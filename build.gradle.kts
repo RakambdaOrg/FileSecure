cca579e19200fc7e97efc91d2cdd87b9f8787a24
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-08-15 10:40:29 +0200

    Bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     java
     application
     alias(libs.plugins.shadow)
-    alias(libs.plugins.names)
     alias(libs.plugins.jib)
 }
 
@@ -80,7 +79,8 @@ tasks {
         archiveClassifier.set("shaded")
         archiveVersion.set("")
 
-        transform(Log4j2PluginsCacheFileTransformer::class.java)
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        transform<Log4j2PluginsCacheFileTransformer>()
     }
 }
 
